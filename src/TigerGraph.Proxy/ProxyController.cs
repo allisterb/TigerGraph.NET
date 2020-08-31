99f2a99f57106724f3d575cf3e8aa4464eab8578
Allister Beharry <allister.beharry@gmail.com>
2020-08-31 13:15:10 -0400

    Add logging for proxy operations.

--- src/TigerGraph.Proxy/ProxyController.cs
+++ src/TigerGraph.Proxy/ProxyController.cs
@@ -6,8 +6,9 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-
+using Serilog;
 using AspNetCore.Proxy;
 using AspNetCore.Proxy.Options;
 
@@ -15,6 +16,12 @@ namespace TigerGraph.Proxy
 {
     public class ProxyController : ControllerBase
     {
+        private static ILogger<ProxyController> log;
+        public ProxyController(ILogger<ProxyController> logger)
+        {
+            log = logger;
+        }
+
         private HttpProxyOptions _httpOptions = HttpProxyOptionsBuilder.Instance
         .WithShouldAddForwardedHeaders(false)
         .WithHttpClientName("TigerGraphClient")
@@ -33,16 +40,19 @@ namespace TigerGraph.Proxy
             hrm.Headers.Add("Access-Control-Allow-Origin", "*");
             hrm.Headers.Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
             hrm.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+            log.LogInformation("Proxy succeeded and Access-Control-* headers added to response.");
             return Task.CompletedTask;
         })
         .WithHandleFailure((c, e) =>
         {
+            log.LogError(e, "Proxy to {0} error.", c.Request.Query);
             return Task.CompletedTask;
         }).Build();
 
         [Route("p/{**rest}")]
         public Task Proxy(string rest)
         {
+            log.LogInformation("Proxying request {0} to {1}...", rest, $"{Environment.GetEnvironmentVariable("TG_SERVER_URL")}/{rest}");
             return this.HttpProxyAsync($"{Environment.GetEnvironmentVariable("TG_SERVER_URL")}/{rest}", _httpOptions);
         }
     }
--- src/TigerGraph.Proxy/Startup.cs
+++ src/TigerGraph.Proxy/Startup.cs
@@ -38,10 +38,7 @@ namespace TigerGraph.Proxy
 
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapGet("/", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello World!");
-                //});
+              
                 app.UseEndpoints(endpoints => endpoints.MapControllers());
             });
         }
